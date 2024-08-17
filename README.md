b3c295d1bf1d95608efe3dada3803d20a8ad7b22
Craig Giles <121510295+CragglesG@users.noreply.github.com>
2024-08-17 14:52:26 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# KnowledgeGraphed
+
+KnowledgeGraphed is an in-development, experimental knowledge graph generator. It converts text data in `.txt` file to a knowledge graph stored in a `.kg` file.
---
+++ make-kg.py
@@ -0,0 +1,29 @@
+print("Starting KnowledgeGraphed...")
+
+import networkx
+import spacy
+import pickle
+
+nlp = spacy.load('en_core_web_sm')
+graph = networkx.Graph()
+
+print("KnowledgeGraphed v0.1.0 (c) Craig Giles 2024")
+print("------------\n")
+
+filename = input("Name of text file: ")
+
+with open(filename, "r") as f:
+    doc = nlp(f.read())
+
+counter=0
+
+for chunk in doc.noun_chunks:
+    graph.add_node(chunk.text)
+    if not counter == 0:
+        graph.add_edge(list(graph.nodes)[counter-1], chunk.text, data=chunk.root.head.text)
+    counter += 1
+
+print("Knowledge", graph, "created!")
+
+with open("graph.kg", "wb") as f:
+    pickle.dump(graph, f)
